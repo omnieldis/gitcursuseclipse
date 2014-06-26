6ab7bb8c05d2da264fd5c82224a480a7fa449254
omnieldis <omnieldis@gmail.com>
2014-06-26 11:08:06 +0200

    Update Main.java

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Bonjour tout le monde");
+		System.out.println("Bonjour tout le gens");
 
 
 	}
